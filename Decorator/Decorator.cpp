aaefc75b04b8ee554784a49f36b77ccd003be8e6
Lerchain <544918380@qq.com>
2019-08-12 23:55:45 +0800

    嵌套类

---
+++ Decorator/Decorator.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class someThing
+{
+	int num;
+	string name;
+public:
+	someThing(string s = "default", int n = 0):num(n),name(s){}
+
+	void doSomeThing()
+	{
+		cout << name << "do" << num << "thing" << endl;
+	}
+};
+class Decorator
+{
+	someThing *s;
+public:
+	Decorator()
+	{
+		s = new someThing();
+	}
+	Decorator(someThing something)
+	{
+		s = new someThing();
+		*s = something;
+	}
+	void doSomething()
+	{
+		cout << "Decorating something" << endl;
+		s->doSomeThing();
+	}
+	~Decorator()
+	{
+		delete s;
+	}
+};
+
+int main()
+{
+	someThing a("I", 1);
+	a.doSomeThing();
+	Decorator A(a);
+	A.doSomething();
+}
+
