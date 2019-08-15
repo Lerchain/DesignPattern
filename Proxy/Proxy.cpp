5b7ccc7132eff0bb767a1303086ac90d1200e967
Lerchain <544918380@qq.com>
2019-08-15 21:12:23 +0800

    代理类，对现有类做修改，易于维护

---
+++ Proxy/Proxy.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+class something
+{
+public:
+	virtual void dosomething() = 0;
+};
+
+class needDoSomething:public something
+{
+public:
+	virtual void dosomething()
+	{
+		cout << "Something need to be done";
+	}
+};
+
+class Proxy:public something
+{
+	needDoSomething sth;
+public:
+	Proxy()
+	{
+	}
+	virtual void dosomething()
+	{
+		cout << "Under Proxy" << endl;
+		sth.dosomething();
+	}
+};
+int main()
+{
+	something *p = new Proxy();
+	p->dosomething();
+}
+
